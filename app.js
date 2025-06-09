8476a9e33bdf8b40cd833669590be3b9accac5f2
matysek42069 <matej.bucinsky@seznam.cz>
2025-06-09 20:55:37 +0200

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const firebaseURL = 'https://your-project-id.firebaseio.com'; // Replace with your Firebase project URL
+const firebaseURL = 'https://console.firebase.google.com/u/0/project/zkouska2-f2653/database/zkouska2-f2653-default-rtdb/data/~2F'; // Replace with your Firebase project URL
 
 const brewingOptions = [
   'Ristretto', 'Espresso', 'Double Espresso', 'Lungo/Americano',
